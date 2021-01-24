144a8992faffdd86e5173bce8ea405188a0cdbbc
bluerhythmz <73447805+bluerhythmz@users.noreply.github.com>
2021-01-23 21:51:36 -0800

    added smooth animations via javascript/css

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const shareArrow = document.querySelector('.share-arrow')
 
 shareBox.addEventListener("click", () => {
     shareButton.classList.toggle('active')
+    
+    if (shareButton.classList.contains('active')) {
+        shareButton.style.animation = ""
+        shareButton.classList.toggle('inactive')
+    } else {
+        shareButton.style.animation = "fadeOut .3s ease forwards";
+        shareButton.style.display = "flex"
+        setTimeout(() => {
+            shareButton.classList.toggle('inactive')
+        }, 1000);
+    }
     shareArrow.classList.toggle('coloredArrow')
     if (document.documentElement.clientWidth >= 415) {
         shareBox.classList.toggle('colored')
